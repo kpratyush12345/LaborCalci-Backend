1d1b501e2bcdf77ed6e4f2547ac2178434a19c8d
Pratyush Kumar <kpratyush12345@gmail.com>
2025-04-29 09:56:56 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres123_7pmq_user:5J6DErYMJEn7ZDPcqWqdu5Oj56DhW2kS@localhost:5432/postgres123_7pmq"
+DATABASE_URL = "postgresql://postgres123_7pmq_user:5J6DErYMJEn7ZDPcqWqdu5Oj56DhW2kS@dpg-d084nbfgi27c7385crig-a:5432/postgres123_7pmq"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
